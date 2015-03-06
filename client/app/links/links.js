493924df9b9bef504e89248edb054e91d6459765
rmagee88 <rmagee88@gmail.com>
2015-03-06 14:56:12 -0800

    Add basic link controller functionality

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,19 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    $http({
+      method: 'GET',
+      url: '/api/links'
+    }).success(function(data, status) {
+      $scope.data.links = data;
+    }).error(function(data, status) {
+      // console.error(data);
+    });
+  };
+
+  $scope.getLinks();
 });
+
